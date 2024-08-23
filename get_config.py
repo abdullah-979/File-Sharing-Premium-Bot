4dd8b3b8b838e03bfe051d0de68874a8e7ab7b38
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-08-23 10:55:48 +0530

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -1,4 +1,4 @@
-import aiohttp
+from requests import get as rget
 import os
 import logging
 
@@ -6,17 +6,15 @@ logging.basicConfig(level=logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL')
+try:
+    if not CONFIG_FILE_URL:
+        raise ValueError("CONFIG_FILE_URL is missing or empty")
 
-if CONFIG_FILE_URL:
-    try:
-        with aiohttp.ClientSession() as session:
-            res = session.get(CONFIG_FILE_URL)
-            res.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
-
-            content = res.read()
-            with open('config.py', 'wb') as f:
-                f.write(content)
-    except Exception as e:
-        LOGGER.error(f"Error downloading CONFIG_FILE_URL: {e}")
-else:
-    LOGGER.error("CONFIG_FILE_URL is missing or empty")
+    res = rget(CONFIG_FILE_URL)
+    if res.status_code == 200:
+        with open('config.py', 'wb+') as f:
+            f.write(res.content)
+    else:
+        LOGGER.error(f"Failed to download config.py {res.status_code}")
+except Exception as e:
+    LOGGER.error(f"Error downloading CONFIG_FILE_URL: {e}")
