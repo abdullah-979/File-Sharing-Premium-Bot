18527d85748a36ee46895398629843e75685cd95
Abdullah <78228041+abdullah-979@users.noreply.github.com>
2025-06-07 17:17:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7940588823:AAHlIGzWYb0-IT14LLtLJi71hAmQnJGOpKc")
+API_ID = int(os.environ.get("API_ID", "2817222"))
+API_HASH = os.environ.get("API_HASH", "aed9f2af23e0df07d7f34011e8a3c86f")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "604152966"))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "filesharingbot")
+OWNER_ID = int(os.environ.get("OWNER_ID", "1994781564"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://cidtamil2:SMietwIr91WmCEf8@cluster0.gbtwinc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "cidtamil2")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002746802139"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001676008024"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
 FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
@@ -22,14 +22,14 @@ PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 try:
-    ADMINS=[604152966]
+    ADMINS=[1994781564]
     for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
@@ -41,9 +41,9 @@ START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention}\n\nI can store p
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {mention}\n\nYou Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 ADMINS.append(OWNER_ID)
-ADMINS.append(604152966)
+ADMINS.append(1994781564)
 
-LOG_FILE_NAME = "filesharingbot.txt"
+LOG_FILE_NAME = "cidtamil2.txt"
 
 logging.basicConfig(
     level=logging.INFO,
